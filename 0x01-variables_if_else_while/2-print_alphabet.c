06b5e8cc4faec4b5cc3148c60d71edee71d02613
Favour okerri <favourokerri767@gmail.com>
2023-02-17 16:13:54 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints lowwer case a
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
