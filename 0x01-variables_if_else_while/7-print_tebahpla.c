cae55aba83706c8d253fc14ee8009a8b2bd3ef3a
Favour okerri <favourokerri767@gmail.com>
2023-02-17 17:39:07 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints in reverse
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'z';
+	while
+		(letter >= 'a') {
+		       putchar(letter);
+		       letter--;
+			}
+	putchar('\n');
+	return (0);
+}
