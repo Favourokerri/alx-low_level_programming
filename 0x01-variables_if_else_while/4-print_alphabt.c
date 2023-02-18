8f424a85e9266fef94d01ff94a43fefd34dc67fc
Favour okerri <favourokerri767@gmail.com>
2023-02-17 17:06:17 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A programm to print and exempt some letters
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+				putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
+
