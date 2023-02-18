37f4ed85d970a6b7af3a6a7f37f5dcf144e8c867
Favour okerri <favourokerri767@gmail.com>
2023-02-18 03:34:54 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,10 @@ int main(void)
 		int digit1;
 		int digit2;
 
-		for (digit1 = 0; digit1 < 9; digit1++)
+		for
+		(digit1 = 0;
+		 digit1 < 9;
+		 digit1++)
 		{
 
 				for (digit2 = digit1 + 1; digit2 < 10; digit2++)
