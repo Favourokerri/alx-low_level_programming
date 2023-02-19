78bed1d19b5bd56978f944eb6295d51beb229f77
Favour okerri <favourokerri767@gmail.com>
2023-02-19 07:00:30 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			putchar((digit1 % 10) + '0');
 			putchar(' ');
 			putchar((digit2 / 10) + '0');
-			putchar((digit2 / 10) + '0');
+			putchar((digit2 % 10) + '0');
 			if (digit1 == 97 && digit2 == 98)
 				continue;
 			putchar(',');
