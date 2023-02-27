54081b124cbd7b68e8b2562f576e373df6bb4288
Favour okerri <favourokerri767@gmail.com>
2023-02-27 11:05:18 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: string to be checked
+ * Return: 0 (success)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	s[] = i;
+
+	for (s[i] = 0; s[i] != '\0'; s[i]++)
+	return (0);
+}
