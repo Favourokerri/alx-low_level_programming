9e0378288784683902f83eb69791d2b1be3d433a
Favour okerri <favourokerri767@gmail.com>
2023-03-20 11:29:47 -0700

    1-pi.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -5,10 +5,10 @@
  * File: 0-object_like_macro.h
  * Auth: Brennan D Baraban
  * Desc: Header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
+ * SIZE: as an abbreviation for the token 1024.
  */
 
 #define SIZE 1024
 
-#endif
+#endif /* OBJECT_LIKE_MACRO_H */
  
