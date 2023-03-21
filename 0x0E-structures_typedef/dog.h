98aba8cb4347817ee11681f3c0cc60a12bde0b4e
Favour okerri <favourokerri767@gmail.com>
2023-03-21 06:32:25 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: all information about the dog
+ */
 struct dog{
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef of struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+oid print_dog(struct dog *d);
 
 #endif
