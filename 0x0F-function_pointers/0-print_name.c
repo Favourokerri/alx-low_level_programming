f978ccbab663a0409c33b48094b57c1d0870a400
Favour okerri <favourokerri767@gmail.com>
2023-03-22 09:39:38 -0700

    functions to pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,45 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_name - function that prints name
+ * @name: name to be printed
+ * @f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
+/**
+ * print_name_lower - prints name in lowercase
+ * @name: name to be printed
+ */
+
+void print_name_lower(char *name)
+{
+	int i;
+
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		_putchar(tolower(name[i]));
+	}
+}
+
+/**
+ * print_name_upper - print name in upper case
+ * @name: name to be printed
+ */
+
+void print_name_upper(char *name)
+{
+	int i;
+
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		_putchar(toupper(name[i]));
+	}
+}
+
