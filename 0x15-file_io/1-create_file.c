ee5a9a1192e2445b8b5300d08ae92ce40b38c229
Favour okerri <favourokerri767@gmail.com>
2023-04-07 04:46:04 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,6 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
-		text_content[len] = '\0';
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0666);
