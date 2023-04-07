af45b004f38d3303699e5cc5fb4bfb8de6fc76d2
Favour okerri <favourokerri767@gmail.com>
2023-04-07 04:18:49 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of the file to be created
+ * @text_content: the content to be written to the file
+ * Return: on success return 1 else -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	int len;
+
+	int byte;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	byte = write(fd, text_content, len);
+
+	if (byte == -1)
+	{
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
