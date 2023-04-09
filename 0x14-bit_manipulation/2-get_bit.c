96343cdcdbc5c44ff335d07d623339450f492b19
Favour okerri <favourokerri767@gmail.com>
2023-04-09 06:24:06 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * get_bit - Gets the value of a bit at index.
+ * @n: The bit to be printed.
+ * @index: The location of the bit to be printed.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - The value of bit at index.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
